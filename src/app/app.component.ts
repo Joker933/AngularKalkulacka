ab157f1a50c145fd99fe8480c815c2dee76277a5
Vojtěch Němec <vojta.nemec933@gmail.com>
2019-12-21 09:00:56 +0100

    RGB!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Kalkulacka'
 }
+
+function vlozit(num: any){
+  document.forms.vysledek.value = document.forms.vysledek.value+num
+}
 //Vubec nevim co zde psat dal :)
