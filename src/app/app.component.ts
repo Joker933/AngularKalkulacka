66560b12ad27ff4561034fbd5b3cd525a50dfd76
Vojtěch Němec <vojta.nemec933@gmail.com>
2019-12-26 22:11:18 +0100

    RipTretak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Kalkulacka'
-  vysledek: any;
-  jedna(){
-    console.log("TEST");
-      this.vysledek = 1;
+  vysledek: any = '';
+  c:number = 0;
+  nula() {
+    this.vysledek += 0;
   }
-  dva(){
-    console.log("TEST2");
-    this.vysledek = 2;
+  jedna() {
+    this.vysledek += 1;
   }
-}
+  dva() {
+    this.vysledek += 2;
+  }
+  tri() {
+    this.vysledek += 3;
+  }
+  ctyri() {
+    this.vysledek += 4;
+  }
+  pet() {
+    this.vysledek += 5;
+  }
+  sest() {
+    this.vysledek += 6;
+  }
+  sedm() {
+    this.vysledek += 7;
+  }
+  osm() {
+    this.vysledek += 8;
+  }
+  devet() {
+    this.vysledek += 9;
+  }
+  plus() {
+    this.vysledek += '+';
+  }
+  minus() {
+    this.vysledek += '-';
+  }
+  krat() {
+    this.vysledek += '*';
+  }
+  deleno() {
+    this.vysledek += '/';
+  }
+  ac() {
+    this.vysledek = '';
+  }
 
-function vlozit(num: any){
-  //document.formular.tabulka.value = document.formular.tabulka.value+nu
-  /*if(num==1){
-    console.log("TEST");
-  }*/
+  vypocet() {
+    this.c = this.vysledek;
+    console.log(this.c);
+    this.vysledek = '';
+  }
+  
+
 }
 //Vubec nevim co zde psat dal :)
