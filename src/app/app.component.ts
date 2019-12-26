46bb57c2a3a7927e4df241f8d8b1216d514f0ec4
Vojtěch Němec <vojta.nemec933@gmail.com>
2019-12-26 15:16:46 +0100

    Citím15%?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Kalkulacka'
+  vysledek: any;
+  jedna(){
+    console.log("TEST");
+      this.vysledek = 1;
+  }
+  dva(){
+    console.log("TEST2");
+    this.vysledek = 2;
+  }
 }
 
-function vlozit(num: BigInteger){
-  document.formular.tabulka.value = document.formular.tabulka.value+num
+function vlozit(num: any){
+  //document.formular.tabulka.value = document.formular.tabulka.value+nu
+  /*if(num==1){
+    console.log("TEST");
+  }*/
 }
 //Vubec nevim co zde psat dal :)
